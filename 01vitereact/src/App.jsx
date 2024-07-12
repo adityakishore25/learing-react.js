30daaa0fc36b0c560f21234668e97e5323bede00
adityakishore25 <adityakishore2512@gmail.com>
2024-07-12 21:29:21 +0530

    third commit

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,13 +3,14 @@
 import Chai from "./chai"
 
 function App() {
+  const userName = "chai aur code"
 
   return (
     // We can return only one element here
     // <></> is called fragments, returning one element only
     <>
       <Chai />
-      <h3>This is built in react + vite</h3>
+      <h3>This is built in react + vite {userName}</h3>
       <p>This a test para</p>
     </>
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,44 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App!! || chai</h1>
+    </div>
+  )
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//     href: 'https://google.com',
+//     target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit
+    google</a>
+)
+
+
+// Below is the correct syntax to create element in react
+const anotherUser = "chai aur react"
+const reactElement = React.createElement(
+  'a',  // tag
+  {  // props object
+    href: 'https://google.com',
+    target: '_blank'
+  },
+  'click me to visit google',  // innerHTML
+  anotherUser // varible ingection
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  reactElement
+
 )
