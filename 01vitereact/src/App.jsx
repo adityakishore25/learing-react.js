98f5fb2d5d7de7d509c22acb7a1a9b46f6e3bea5
adityakishore25 <adityakishore2512@gmail.com>
2024-07-12 15:59:13 +0530

    second commit

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,18 @@
 
+
+import Chai from "./chai"
+
 function App() {
 
   return (
-    <h1>Chai aur react with vite || Aditya Kishore</h1>
+    // We can return only one element here
+    // <></> is called fragments, returning one element only
+    <>
+      <Chai />
+      <h3>This is built in react + vite</h3>
+      <p>This a test para</p>
+    </>
+
   )
 }
 
