d021b9719aa887a41ab19cde8965367d80125511
adityakishore25 <adityakishore2512@gmail.com>
2024-07-12 23:17:16 +0530

    fourth commit

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // Hooks :
+  let [counter, setCounter] = useState(15)  // returns two values, storing in array
+
+
+  // let counter = 15
+
+  const addValue = () => {
+    counter++
+    setCounter(counter)
+    console.log("value added", counter);
+  }
+  const removeValue = () => {
+    counter--
+    setCounter(counter)
+    console.log("value added", counter);
+  }
+  return (
+    <>
+      <h1>chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+      <button
+      onClick={addValue}
+      >Add value {counter}</button>
+      <br />
+      <button
+      onClick={removeValue}
+      >remove value: {counter}</button>
+      <h3>footer : {counter}</h3>
+    </>
+  )
+}
+
+export default App
